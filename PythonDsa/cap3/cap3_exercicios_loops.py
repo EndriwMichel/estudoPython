00b15701c4fd25b9bdd4a67642353608a550e80a
Endriw <endriwmichel@gmail.com>
2019-08-15 23:59:47 -0300

    primeiro commit dos capitulos 2 e 3

---
+++ PythonDsa/cap3/cap3_exercicios_loops.py
@@ -0,0 +1,2 @@
+# Exercício 1 - Crie uma estrutura que pergunte ao usuário qual o dia da semana. Se o dia for igual a Domingo ou 
+# igual a sábado, imprima na tela "Hoje é dia de descanso", caso contrário imprima na tela "Você precisa trabalhar!"
