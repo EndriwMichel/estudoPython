7aff74766eaa3148c403d0565bdbd522f37a8178
Endriw <endriwmichel@gmail.com>
2019-08-19 23:28:19 -0300

    Finalizado exercicios do capitulo 3

--- PythonDsa/cap3/cap3_exercicios_funcoes.py
+++
@@ -1,7 +0,0 @@
-# Exercício 1 - Crie uma função que imprima a sequência de números pares entre 1 e 20 (a função não recebe parâmetro) e 
-# depois faça uma chamada à função para listar os números
-def imprimeNumeros():
-    for i in range(1, 21):
-        print(i) 
-
-imprimeNumeros()
