4111daf666a35c58aeefd43cfcd173f78036f0da
Endriw <endriwmichel@gmail.com>
2019-09-09 23:12:14 -0300

    Exercicios cap4 em andamento

--- PythonDsa/cap4/Exercicios/exercicios_cap4.py
+++ PythonDsa/cap4/Exercicios/exercicios_cap4.py
@@ -8,15 +8,34 @@
 # palavras = 'A Data Science Academy oferce os melhores cursos de análise de dados do Brasil'.split()
 # resultado = [[w.upper(), w.lower(), len(w)] for w in palavras]
 # for i in resultado:
-#     print (i)
+#     print(i)
 
-palavras = 'A Data Science Academy oferce os melhores cursos de análise de dados do Brasil'.split()
+# palavras = 'A Data Science Academy oferce os melhores cursos de análise de dados do Brasil'.split()
 
-    print(type(valor))
-def loadString(valor):
-    for i in valor:
-        resultado = [i.upper(), i.lower(), len(i)]
-    return resultado
+# def loadString(valor):
+#     resultado = [valor.upper(), valor.lower(), len(valor)]
+#     return print(resultado)
 
-print(palavras[2])
-print(list(map(loadString, palavras)))
+# list(map(loadString, palavras))
+
+
+# Exercício 3 - Calcule a matriz transposta da matriz abaixo.
+# Caso não saiba o que é matriz transposta, visite este link: https://pt.wikipedia.org/wiki/Matriz_transposta
+# Matriz transposta é um conceito fundamental na construção de redes neurais artificiais, base de sistemas de IA.
+matrix = [[1, 2],[3,4],[5,6],[7,8]]
+
+vet1 = []
+vet2 = []
+aux = []
+
+for x, y in matrix:
+    vet1.append(x)
+    vet2.append(y)
+
+aux.append(vet1)
+aux.append(vet2)
+
+for i in aux:
+    print(i)
+
+# print(aux)
