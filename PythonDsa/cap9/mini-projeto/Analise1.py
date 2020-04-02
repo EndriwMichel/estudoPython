eb82d7e1891c54de04b61edc3c8cdbbec000d262
endriw <endriwmichel@gmail.com>
2020-04-02 10:28:07 -0300

    continuando analise 1 dos exercicios do capitulo 9

--- PythonDsa/cap9/mini-projeto/Analise1.py
+++ PythonDsa/cap9/mini-projeto/Analise1.py
@@ -1,8 +1,31 @@
 # Analise 1 miniprojeto cap 9
+# database: https://www.kaggle.com/orgesleka/used-cars-database
 import pandas as pd
 import zipfile
+import os
+import subprocess
+import stat
+import numpy as np
+import pandas as pd
+# import seaborn as sns
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 zip_cars = zipfile.ZipFile("autos.zip", 'r')
 df = pd.read_csv(zip_cars.open('autos.csv'), encoding="latin-1")
 
-print(df)
+"""
+Crie um Plot com a Distribuição de Veículos com base no Ano de Registro
+"""
+# plt.hist(df.yearOfRegistration, density=True, histtype='stepfilled')
+# plt.show()
+
+"""
+Crie um Boxplot para avaliar os outliers
+"""
+fig1, ax1 = plt.subplots()
+ax1.set_title('Basic Plot')
+ax1.boxplot([df.price, df.vehicleType])
+plt.show()
+# print(df)
+# cd .\Documents\estudoPython\PythonDsa\cap9\mini-projeto\
